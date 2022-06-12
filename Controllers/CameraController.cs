0256fbf062280eaec5e2b529afe0cda54d41650e
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2022-06-12 16:24:25 -0500

    added camera api project

---
+++ Controllers/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CameraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CameraController : ControllerBase
+    {
+        public CameraController()
+        {
+        }
+
+        private readonly ProducerConfig config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        private readonly string topic = "receipt-image";
+        [HttpPost("/upload")]
+        public async Task<IActionResult> Upload(string image)
+        {
+            try
+            {
+                var result = await PublishMessage(topic, image);
+                return Ok(result);
+            }
+            catch(Exception)
+            {
+                return Problem("Could not upload the image");
+            }
+        }
+
+        private async Task<object> PublishMessage(string topic, string message)
+        {
+            using(var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                    return result;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+    }
+}
